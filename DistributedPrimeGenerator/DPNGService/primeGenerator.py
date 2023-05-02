3c6a921c35b6b7dd01470260a07480e19506363c
papadoxie <nofilqasim@gmail.com>
2023-05-03 04:13:39 +0500

    venv stuff updated

--- DistributedPrimeGenerator/DPNGService/primeGenerator.py
+++ DistributedPrimeGenerator/DPNGService/primeGenerator.py
@@ -4,18 +4,17 @@ class PrimeGenerator:
     primes = []
 
     def start(self, start, end):
-        is_prime = [True] * (end+1)
-        is_prime[0], is_prime[1] = False, False
-        
-        for i in range(2, int(end**0.5)+1):
-            if is_prime[i]:
+        for i in range(start, end + 1):
+            if self.isPrime(i):
                 self.primes.append(i)
-                for j in range(i**2, end+1, i):
-                    is_prime[j] = False
-            
-        for i in range(int(end **0.5) + 1, end+1):
-            if is_prime[i]:
-                self.primes.append(i)
-            
+         
+    def isPrime(self, candidate):
+        if candidate < 2:
+            return False
+        for i in range(2, int(candidate ** 0.5) + 1):
+            if candidate %  i == 0:
+                return False
+        return True    
+           
     def get(self):
         return self.primes
